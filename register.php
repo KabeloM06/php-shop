b709c00580815cc813b37a74b159e5e40048a424
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-12-21 12:17:55 +0200

    Real user id now in register.php & place_order.php

--- register.php
+++ register.php
@@ -46,6 +46,8 @@ if(isset($_POST['register'])){
 
     //if accout is created successfully
     if($stmt->execute()){
+        $user_id = $stmt->insert_id;
+        $_SESSION['user_id'] = $user_id;
         $_SESSION['user_email'] = $email;
         $_SESSION['user_name'] = $name;
         $_SESSION['logged_in'] = true;
